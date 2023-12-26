4895d88e5e71b718c1ee4c37be84a5e471aa18a0
IvanTimonin08 <149607451+IvanTimonin08@users.noreply.github.com>
2023-12-26 15:50:32 +0300

    FastapiTimonin.py

--- FastapiTimonin.py
+++ FastapiTimonin.py
@@ -1,26 +1,30 @@
-from transformers import ViTImageProcessor, ViTForImageClassification
-from PIL import Image
+from transformers import ViTFeatureExtractor, ViTForImageClassification
 import requests
 from fastapi import FastAPI
-from typing import Optional
+from PIL import Image
+from io import BytesIO
 
 app = FastAPI()
 
+# Загрузка процессора и модели ViT
+processor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224')
+model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
+
 @app.get("/predict_image_class")
-async def predict_image_class(http://127.0.0.1:8000/predict_image_class?image_url=http://images.cocodataset.org/val2017/000000039769.jpg):
-    # Загрузка изображения по предоставленному URL
-    image = Image.open(requests.get(image_url, stream=True).raw)
+async def predict_image_class(image_url: str):
+    try:
+        # Загрузка изображения по предоставленному URL
+        response = requests.get(image_url)
+        image = Image.open(BytesIO(response.content))
 
-    # Загрузка процессора и модели ViT
-    processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
-    model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
+        # Обработка изображения и выполнение предсказаний
+        inputs = processor(images=image, return_tensors="pt")
+        outputs = model(**inputs)
+        logits = outputs.logits
+        # Модель предсказывает один из 1000 классов ImageNet
+        predicted_class_idx = logits.argmax(-1).item()
+        predicted_class = model.config.id2label[predicted_class_idx]
 
-    # Обработка изображения и выполнение предсказаний
-    inputs = processor(images=image, return_tensors="pt")
-    outputs = model(**inputs)
-    logits = outputs.logits
-    # Модель предсказывает один из 1000 классов ImageNet
-    predicted_class_idx = logits.argmax(-1).item()
-    predicted_class = model.config.id2label[predicted_class_idx]
-
-    return {"predicted_class": predicted_class}
+        return {"predicted_class": predicted_class}
+    except Exception as e:
+        return {"error": str(e)}
