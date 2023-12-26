591dbd06bb1aacb9a259942f5be0deb6f999b14e
IvanTimonin08 <149607451+IvanTimonin08@users.noreply.github.com>
2023-12-26 15:02:08 +0300

    FastapiTimonin.py

---
+++ FastapiTimonin.py
@@ -0,0 +1,26 @@
+from transformers import ViTImageProcessor, ViTForImageClassification
+from PIL import Image
+import requests
+from fastapi import FastAPI
+from typing import Optional
+
+app = FastAPI()
+
+@app.get("/predict_image_class")
+async def predict_image_class(image_url: str):
+    # Загрузка изображения по предоставленному URL
+    image = Image.open(requests.get(image_url, stream=True).raw)
+
+    # Загрузка процессора и модели ViT
+    processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
+    model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
+
+    # Обработка изображения и выполнение предсказаний
+    inputs = processor(images=image, return_tensors="pt")
+    outputs = model(**inputs)
+    logits = outputs.logits
+    # Модель предсказывает один из 1000 классов ImageNet
+    predicted_class_idx = logits.argmax(-1).item()
+    predicted_class = model.config.id2label[predicted_class_idx]
+
+    return {"predicted_class": predicted_class}
