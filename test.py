34ea275e704a29a4f7810fa7c33e62feec443498
IvanTimonin08 <149607451+IvanTimonin08@users.noreply.github.com>
2023-12-28 00:37:30 +0300

    test.py

---
+++ test.py
@@ -0,0 +1,21 @@
+import pytest
+import httpx
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_predict_image_class():
+    # Создание фиктивного файла изображения для теста
+    image_data = b'\xff\xd8\xff\xe0\x00\x10JFIF\x00\x01\x01\x01\x00`\x00`\x00\x00\xff\xdb\x00C\x00\x08\x06\x06...'
+    files = {'image': ('test_image.jpg', image_data, 'image/jpeg')}
+
+    # Отправка POST-запроса к эндпоинту /predict_image_class
+    response = client.post("/predict_image_class", files=files)
+
+    # Проверка кода ответа
+    assert response.status_code == 200
+
+    # Проверка содержимого ответа
+    data = response.json()
+    assert "predicted_class" in data
