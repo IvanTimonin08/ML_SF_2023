f0c13b09df828da4ff934b8a52bf407f4334bf07
IvanTimonin08 <149607451+IvanTimonin08@users.noreply.github.com>
2024-01-22 18:51:00 +0300

    test_FastapiTimonin.py

--- test_FastapiTimonin.py
+++ test_FastapiTimonin.py
@@ -1,72 +1,156 @@
-import pytest
-import torch
-import torchvision
-import torchvision.transforms as transforms
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
+#-----------------------------------
+# TRAINING OUR MODEL
+#-----------------------------------
+import h5py
+import numpy as np
+import os
+import glob
+import cv2
+import warnings
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import KFold, StratifiedKFold
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.externals import joblib
 
-# Загрузка и предобработка данных
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+warnings.filterwarnings('ignore')
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=2)
+#--------------------
+# tunable-parameters
+#--------------------
+num_trees = 100
+test_size = 0.10
+seed      = 9
+train_path = "dataset/train"
+test_path  = "dataset/test"
+h5_data    = 'output/data.h5'
+h5_labels  = 'output/labels.h5'
+scoring    = "accuracy"
 
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+# get the training labels
+train_labels = os.listdir(train_path)
 
-# Определение нейронной сети
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+# sort the training labels
+train_labels.sort()
 
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+if not os.path.exists(test_path):
+    os.makedirs(test_path)
 
-net = Net()
+# create all the machine learning models
+models = []
+models.append(('LR', LogisticRegression(random_state=seed)))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier(random_state=seed)))
+models.append(('RF', RandomForestClassifier(n_estimators=num_trees, random_state=seed)))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(random_state=seed)))
 
-# Определение функции потерь и оптимизатора
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+# variables to hold the results and names
+results = []
+names   = []
 
-# Тест классификации изображений
-def test_image_classification():
-    for epoch in range(2):  # Проход по данным несколько раз
+# import the feature vector and trained labels
+h5f_data  = h5py.File(h5_data, 'r')
+h5f_label = h5py.File(h5_labels, 'r')
 
-        running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
-            inputs, labels = data
+global_features_string = h5f_data['dataset_1']
+global_labels_string   = h5f_label['dataset_1']
 
-            optimizer.zero_grad()
+global_features = np.array(global_features_string)
+global_labels   = np.array(global_labels_string)
 
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
+h5f_data.close()
+h5f_label.close()
 
-            running_loss += loss.item()
-            if i % 2000 == 1999:    # печать статистики каждые 2000 мини-пакетов
-                print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 2000))
-                running_loss = 0.0
+# verify the shape of the feature vector and labels
+print("[STATUS] features shape: {}".format(global_features.shape))
+print("[STATUS] labels shape: {}".format(global_labels.shape))
 
-    print('Обучение завершено')
+print("[STATUS] training started...")
+
+# split the training and testing data
+(trainDataGlobal, testDataGlobal, trainLabelsGlobal, testLabelsGlobal) = train_test_split(np.array(global_features),
+                                                                                          np.array(global_labels),
+                                                                                          test_size=test_size,
+                                                                                          random_state=seed)
+
+print("[STATUS] splitted train and test data...")
+print("Train data  : {}".format(trainDataGlobal.shape))
+print("Test data   : {}".format(testDataGlobal.shape))
+print("Train labels: {}".format(trainLabelsGlobal.shape))
+print("Test labels : {}".format(testLabelsGlobal.shape))
+
+# 10-fold cross validation
+for name, model in models:
+    kfold = KFold(n_splits=10, random_state=seed)
+    cv_results = cross_val_score(model, trainDataGlobal, trainLabelsGlobal, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+# boxplot algorithm comparison
+fig = pyplot.figure()
+fig.suptitle('Machine Learning algorithm comparison')
+ax = fig.add_subplot(111)
+pyplot.boxplot(results)
+ax.set_xticklabels(names)
+pyplot.show()
+
+#-----------------------------------
+# TESTING OUR MODEL
+#-----------------------------------
+
+# to visualize results
+import matplotlib.pyplot as plt
+
+# create the model - Random Forests
+clf  = RandomForestClassifier(n_estimators=num_trees, random_state=seed)
+
+# fit the training data to the model
+clf.fit(trainDataGlobal, trainLabelsGlobal)
+
+# loop through the test images
+for file in glob.glob(test_path + "/*.jpg"):
+    # read the image
+    image = cv2.imread(file)
+
+    # resize the image
+    image = cv2.resize(image, fixed_size)
+
+    ####################################
+    # Global Feature extraction
+    ####################################
+    fv_hu_moments = fd_hu_moments(image)
+    fv_haralick   = fd_haralick(image)
+    fv_histogram  = fd_histogram(image)
+
+    ###################################
+    # Concatenate global features
+    ###################################
+    global_feature = np.hstack([fv_histogram, fv_haralick, fv_hu_moments])
+
+    # scale features in the range (0-1)
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    rescaled_feature = scaler.fit_transform(global_feature)
+
+    # predict label of test image
+    prediction = clf.predict(rescaled_feature.reshape(1,-1))[0]
+
+    # show predicted label on image
+    cv2.putText(image, train_labels[prediction], (20,30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,255,255), 3)
+
+    # display the output image
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    plt.show()
 
 # Запуск теста
 test_image_classification()
